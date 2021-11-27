c70d82cd6b2c858921e61192d556fc0fb2e8a2e0
JaeSung Choi <ky4171@naver.com>
2021-11-27 13:44:38 +0900

    초기화면

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
+//const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
+const nunjucks = require('nunjucks');
+const passport = require('passport');
 
 dotenv.config();
 const app = express();
+const initPage = require('./routes/init');
+
+
 app.set('port',process.env.PORT || 8052);
+app.set('view engine','html');
+nunjucks.configure('views',{
+    express: app,
+    watch: true,
+});
 
 app.use(morgan('dev'));
-app.use('/',express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+/*app.use(session({
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
@@ -23,8 +33,12 @@ app.use(session({
         secure: false,
     },
     name: 'session-cookie',
-}));
+}));*/
+app.use(passport.initialize());
+//app.use(passport.session());
 
-app.use((req,res,next) =>{
-    next();
+app.use('/',initPage);
+
+app.listen(app.get('port'),() =>{
+    console.log(app.get('port'));
 })
